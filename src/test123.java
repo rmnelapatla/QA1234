d051b14ce3001088cb80c85531142b85c8f227f7
ramana <rmnelapatla@users.noreply.github.com>
2023-11-17 07:17:05 +0530

    Update test123.java
    Change done from repo

--- src/test123.java
+++ src/test123.java
@@ -2,7 +2,12 @@ package src;
 
 public class test123 {
 
-    public static void main(String r[]){
+    public static void main(String arg[]){
+        System.Out.Println("this is from repo");
+        // new line added
 
     }
+
+    public void getsum(int a, int b){
+        System.out.println(a+b);
 }
