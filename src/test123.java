591c3f273b433aa91e0aea762fcf593cd3d5cc22
rmnelapatla70 <rama_70@yahoo.com>
2023-11-17 07:24:39 +0530

    merged code

--- src/test123.java
+++ src/test123.java
@@ -3,13 +3,14 @@ package src;
 public class test123 {
 
 
-            public static void main(String arg[]){
-                System.out.println("welcome to GIT Test");
-                System.Out.Println("this is from repo");
+    public static void main(String arg[]) {
+        System.out.println("welcome to GIT Test");
+        System.out.println("this is from repo");
         // new line added
 
     }
 
-    public void getsum(int a, int b){
-        System.out.println(a+b);
+    public void getsum(int a, int b) {
+        System.out.println(a + b);
+    }
 }
